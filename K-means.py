7ed8e300b5f7ff9867049b8093ce7b59570f0300
claudiazi <claudiazi1244@gmail.com>
2021-03-15 22:06:07 +0100

    Add notes of lesson 4

--- K-means.py
+++ K-means.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from functools import partial
 from pprint import pprint
+from random import sample
 from typing import Iterable, Tuple, List, Sequence, Dict
 from math import fsum, sqrt
 
@@ -8,14 +9,6 @@ Point = Tuple[int, ...]
 Centroid = Point
 PointGroup = Sequence[Point]
 
-points = [
-    (10, 41, 23),
-    (22, 30, 29),
-    (11, 42, 5),
-    (20, 32, 4),
-    (12, 40, 12),
-    (21, 36, 23),
-]
 
 
 def mean(data: Iterable[float]) -> float:
@@ -43,7 +36,28 @@ def compute_centroids(groups: Iterable[PointGroup]) -> List[Centroid]:
     return [tuple(map(mean, zip(*group))) for group in groups]   #MyPy doesnt know how to deal with 'zip(*)' yet
 
 
+def k_means(data: Iterable[Point], k: int = 2, iterations: int = 50) ->List[Centroid]:
+    data = list(data)
+    centroids = sample(data, k)
+    for i in range(iterations):
+        labeled = assign_data(centroids, data)
+        centroids = compute_centroids(labeled.values())
+    return centroids
+
+
 if __name__ == "__main__":
+    points = [
+        (10, 41, 23),
+        (22, 30, 29),
+        (11, 42, 5),
+        (20, 32, 4),
+        (12, 40, 12),
+        (21, 36, 23),
+    ]
+    centroids = k_means(points, k=3)
+    d = assign_data(centroids, points)
+    pprint(d)
+    """
     for point in points:
         print(point, dist(point, (9, 39, 20)))
 
@@ -53,3 +67,4 @@ if __name__ == "__main__":
     print(min(centroids, key=lambda centroid: dist(point, centroid)))
     print(min(centroids, key=partial(dist, point)))
     pprint(assign_data(centroids, points), width=45)
+    """
